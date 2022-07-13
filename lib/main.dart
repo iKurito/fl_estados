47d32add623cbfeb96da1329c6f9d54d233ceb2b
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2022-07-12 19:24:23 -0500

    gestor de estados inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:fl_estados/bloc/user/user_bloc.dart';
 import 'package:flutter/material.dart';
 
 import 'package:fl_estados/pages/pages.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,21 +9,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<UserBloc>(
-          create: (context) => UserBloc(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        initialRoute: 'pagina1',
-        routes: {
-          'pagina1': (_) => const Pagina1Page(),
-          'pagina2': (_) => const Pagina2Page(),
-        }
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'pagina1',
+      routes: {
+        'pagina1': (_) => const Pagina1Page(),
+        'pagina2': (_) => const Pagina2Page(),
+      }
     );
   }
 }
--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,7 +1,4 @@
-import 'package:fl_estados/bloc/user/user_bloc.dart';
-import 'package:fl_estados/models/user.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Pagina1Page extends StatelessWidget {
    
@@ -12,20 +9,8 @@ class Pagina1Page extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pagina 1'),
-        actions: [
-          IconButton( 
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () => BlocProvider.of<UserBloc>(context, listen: false).add(DeleteUser()),
-          )
-        ],
       ),
-      body: BlocBuilder<UserBloc, UserState>(
-        builder: (context, state) {
-          return state.existUser
-            ? InformacionUsuario(user: state.user!)
-            : const Center(child: Text('No hay usuario seleccionado'));
-        },
-      ),
+      body: const InformacionUsuario(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.accessibility_new),
         onPressed: () => Navigator.pushNamed(context, 'pagina2'),
@@ -36,11 +21,8 @@ class Pagina1Page extends StatelessWidget {
 
 class InformacionUsuario extends StatelessWidget {
   const InformacionUsuario({
-    Key? key,
-    required this.user
+    Key? key
   }) : super(key: key);
-
-  final User user;
 
   @override
   Widget build(BuildContext context) {
@@ -52,14 +34,13 @@ class InformacionUsuario extends StatelessWidget {
         physics: const BouncingScrollPhysics(),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const Divider(),
-            ListTile(title: Text('Nombre: ${user.nombre}')),
-            ListTile(title: Text('Edad: ${user.edad}')),
-            const Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const Divider(),
-            ...user.profesiones.map((profesion) => ListTile(title: Text(profesion))),
+          children: const [
+            Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Divider(),
+            ListTile(title: Text('Nombre: ')),
+            ListTile(title: Text('Edad: ')),
+            Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Divider(),
           ],
         ),
       )
